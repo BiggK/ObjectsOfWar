409bb55669590bef7437b154c6cef90fac078a13
BigK <sirz2u@gmail.com>
2013-02-04 22:52:47 -0600

    Even more cleanup

--- proj.android/src/com/killian/objectsofwar/ObjectsOfWar.java
+++ proj.android/src/com/killian/objectsofwar/ObjectsOfWar.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 public class ObjectsOfWar extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+	
+    static {
+         System.loadLibrary("objectsofwar");
     }
 }
